a7a2c0d7cc563a67d7b6952c6de1e847230a53b3
TVG123456 <tushargujrathi30592@gmail.com>
2023-02-15 14:11:37 +0530

    added all files

---
+++ interface.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request, render_template
+import config
+from utilities import Diabetic
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Diabetic Prediction App"
+
+@app.route("/predict", methods = ["Post"])
+def get_prediction():
+    data = request.form
+    glucose = eval(data["g"])
+    bloodpressure = eval(data["bp"])
+    skintickness = eval(data["st"])
+    insulin = eval(data["ins"])
+    bmi = eval(data["bmi"])
+    diabeticpfunction = eval(data["dpf"])
+    age = eval(data["age"])
+
+    diabetes_test = Diabetic(glucose,bloodpressure,skintickness,insulin,bmi,diabeticpfunction,age)
+    prediction = diabetes_test.predict_diabetes()
+    print("*************************")
+    print(prediction)
+    print("*************************")
+
+    return jsonify({"Result":prediction})
+    #return render_template("index.html", result=prediction)
+
+if __name__ == "__main__":
+    app.run(debug = True, port = config.PORT_NUMBER, host = config.HOST)
